9028c1f8ea8470f9023c51375fb6fb33cf86b8a4
matrix2992007 <yosefbebo392@gmail.com>
2026-02-06 01:27:59 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# حط هنا بياناتك الحقيقية
+MAIN_BOT_TOKEN = "7547195483:AAGY_P6I3-fR8tG0K_3qU0xQ0m6N9Gk-VfM" # توكين بوت تريند كاش
+ADMIN_BOT_TOKEN = "7958432101:AAH-7m1n_k_X1qVzY0r5T8p_m9O6L8k-Z" # توكين بوت الإشعارات
+ADMIN_ID = 8303478267 # الآيدي بتاعك
+MIN_WITHDRAW = 10.0 # الحد الأدنى
+FEE = 1.0 # الجنيه الخصم
